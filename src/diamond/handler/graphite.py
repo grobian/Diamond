f3a4c09dbff75292eea98140abcd9f04b6851d83
Fabian Groffen <grobian@gentoo.org>
2020-04-26 14:31:00 +0200

    graphite: send data as bytes
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- src/diamond/handler/graphite.py
+++ src/diamond/handler/graphite.py
@@ -128,7 +128,7 @@ class GraphiteHandler(Handler):
         Try to send all data in buffer.
         """
         try:
-            self.socket.sendall(data)
+            self.socket.sendall(str.encode(data))
             self._reset_errors()
         except:
             self._close()
@@ -136,7 +136,7 @@ class GraphiteHandler(Handler):
                                  "trying reconnect.")
             self._connect()
             try:
-                self.socket.sendall(data)
+                self.socket.sendall(str.encode(data))
             except:
                 return
             self._reset_errors()
